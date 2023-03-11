14e60354badc8b9e483c6a6b5ae275b53b9ec97a
Sunay Bulut <bulutcayanmunzur@gmail.com>
2023-03-12 02:01:07 +0300

    selection_sort_projesi1py

---
+++ # Selection_Sort_Projesi 1.py
@@ -0,0 +1,33 @@
+# Selection_Sort_Projesi
+
+#Proje 1
+
+1. [ 22, 27, 16, 2, 18, 6]
+
+##Verilen dizinin sort türüne göre aşaması
+
+ Önce dizinin küçükten büyüye sıraya koymak için dizinin içerisinde en küçük sayıyı bulmak gerekiyor 22, 27den küçük o zaman devam ediyoruz 16 dur ve sola al
+
+2. [16, 22, 27, 2, 18, 6]
+
+ ##16 yı başa aldık ve 22 den sonra en küçük 2 küçük alıyoruz başa 
+
+3. [ 2, 16, 22, 27, 18, 6]
+
+ ##Kalan sayılarda devam ediyoruz ilk küçük 18 ve başa alıyoruz 
+
+4. [2, 16, 18, 22, 27]
+
+##Big-O : O (n^2)
+
+##Time Complexity : 18 dizinin ortasında olduğu için Average Case kapsamına girer
+
+##[7,3,5,8,2,9,4,15,6] dizisinin Selection Sort'a göre ilk 4 adımı
+
+1. [2,3,5,8,7,9,4,15,6]
+
+2. [2,3,5,8,7,9,4,15,6]
+
+3. [2,3,4,8,7,9,5,15,6] 
+
+4. [2,3,4,5,7,9,8,15,6]
